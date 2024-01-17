79b8f304892ce3fdda22a741c6553236d5785ed2
Alexandros Karagiannis <pp.alexkara@gmail.com>
2024-01-16 17:11:19 -0700

    chore(example): added example

--- examples/simple-example/src/App.tsx
+++ examples/simple-example/src/App.tsx
@@ -1,11 +1,10 @@
 // Releasing of scoped packages isn't free, thus using different names for developing & usage
 // Also in packages\password\package.json:name=@form-validation/password then perform npm install in root folder for the namespaces to take effect
 // import { PasswordForm } from "@form-validation/password";
-// import { PasswordForm } from "akaragiannis-password-form-validation";
+import { PasswordForm } from "akaragiannis-password-form-validation";
 
 function App() {
-  // return <PasswordForm />;
-  return <div></div>;
+  return <PasswordForm />;
 }
 
 export default App;
